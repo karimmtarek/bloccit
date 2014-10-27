c989a5bf642df5960568fe916d1847d8dc7c0cb1
Karim Tarek <karimmtarek@gmail.com>
2014-10-27 00:30:49 -0400

    Implemented user profile page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,8 @@ class Post < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  # after_create :create_vote
-
-  # default_scope {order('created_at DESC')}
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
