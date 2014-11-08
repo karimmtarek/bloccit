1c4e8324761295d6000fb9d11ebfe78604e548bb
Matthieu Tanguay-Carel <matthieutc@gmail.com>
2014-11-08 11:55:29 +0100

    defaults to test keys for fog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'] || 'test',
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'] || 'test'
   }
   config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_public     = true
